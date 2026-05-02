653ff7fa9c50bfc1acd06f81d96e70c9eee3197b
Sebastian S. <55443058+mayloo89@users.noreply.github.com>
2026-05-02 17:59:59 -0300

    Allow photon.komoot.io in CSP for location autocomplete (#81)
    * Add NEXT_PUBLIC_IMAGE_UNOPTIMIZED build arg to bypass _next/image on Pi
    
    * Allow photon.komoot.io in CSP connect-src for location autocomplete

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -40,7 +40,7 @@ const securityHeaders = [
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "img-src 'self' data: blob: https:",
             "font-src 'self' https://fonts.gstatic.com",
-            "connect-src 'self' wss: https://cdn.growthbook.io",
+            "connect-src 'self' wss: https://cdn.growthbook.io https://photon.komoot.io",
             "frame-ancestors 'none'",
           ].join("; "),
         },
